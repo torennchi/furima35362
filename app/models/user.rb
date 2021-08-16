5cfe90b38ea2c94230869793c8a1b59db52ce73e
torennchi <xiangximachang@gmail.com>
2021-08-16 18:34:35 +0900

    ログイン、ログアウト機能を実装する

---
+++ app/models/user.rb
@@ -0,0 +1,18 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  
+  validates :email              , presence: true
+  validates :encrypted_password , presence: true
+  validates :nickname           , presence: true
+  validates :birthday           , presence: true
+  validates :surname            , presence: true
+  validates :name               , presence: true
+  validates :kana_surname       , presence: true
+  validates :kana_name          , presence: true
+
+  has_many :items
+  has_many :buyer_records
+end
