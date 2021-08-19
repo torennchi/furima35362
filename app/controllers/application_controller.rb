785697e0c5d32f2cf88986b061daec0bde1d5857
torennchi <xiangximachang@gmail.com>
2021-08-19 17:44:48 +0900

    ログイン機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,6 @@ class ApplicationController < ActionController::Base
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :surname, :name, :kana_surname, :kana_name,])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :birthday, :surname, :name, :kana_surname, :kana_name])
   end
 end
