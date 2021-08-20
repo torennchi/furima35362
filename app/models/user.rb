69d2b375370b00f248301968d128920782910652
torennchi <xiangximachang@gmail.com>
2021-08-20 17:40:09 +0900

    修正箇所を修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
          with_options presence: true do
-          validates :password, format:{ with:/\A[a-zA-Z0-9]+\z/, message: "is invalid. Input half-width characters."}
-          validates :surname, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-          validates :name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-          validates :nickname, format: { with: /\A[a-z0-9]+\z/i, message: "is invalid. Input half-width characters."}
+          validates :password, format:{ with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "is invalid. Input half-width characters."}
+          validates :surname, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+          validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
+          validates :nickname
           validates :kana_surname, format: { with:/\A[ァ-ヶー－]+\z/, message: "is invalid."}
           validates :kana_name, format: { with:/\A[ァ-ヶー－]+\z/, message: "is invalid."}
           validates :birthday
