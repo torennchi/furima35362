1aecfa001fa5dae82a417402ca00fb76b81d276a
torennchi <xiangximachang@gmail.com>
2021-08-22 21:55:07 +0900

    商品出品情報を保存するためのモデルとテーブルを作成する

---
+++ db/migrate/20210822124706_create_items.rb
@@ -0,0 +1,18 @@
+class CreateItems < ActiveRecord::Migration[6.0]
+  def change
+    create_table :items do |t|
+      t.string :product_name,              null: false
+      t.integer :category_id, null: false
+      t.integer :price,           null: false
+      t.references :user,           foreign_key: true
+      t.text :explanation,            null: false
+      t.integer :status_id,               null: false
+      t.integer :cost_id,       null: false
+      t.integer :area_id,          null: false
+      t.integer :days_id,          null: false
+
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_141557) do
+ActiveRecord::Schema.define(version: 2021_08_22_124706) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -31,19 +31,6 @@ ActiveRecord::Schema.define(version: 2021_08_11_141557) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
-  end
-
-  create_table "articles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "title", null: false
-    t.text "text", null: false
-    t.integer "genre_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "genres", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
