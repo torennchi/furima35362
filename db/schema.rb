89c516cb38f003a514709e6bd1748fcc5f9341fe
torennchi <xiangximachang@gmail.com>
2021-08-22 22:02:12 +0900

    Active Storageの導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_22_124706) do
+ActiveRecord::Schema.define(version: 2021_08_22_125757) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
