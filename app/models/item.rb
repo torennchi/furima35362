1f73f47cd84389808eb867b34c44c61d1caa1dd6
torennchi <xiangximachang@gmail.com>
2021-09-01 12:24:12 +0900

    修正2

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,16 +11,8 @@ class Item < ApplicationRecord
   has_one :buyer_record
   has_one_attached :image
 
-  # validates :product_name, :text, presence: true
-  # validates :area_id, numericality: { other_than: 0 , message: "can't be blank"}
-  # validates :category_id, numericality: { other_than: 0 , message: "can't be blank"}
-  # validates :cost_id, numericality: { other_than: 0 , message: "can't be blank"}
-  # validates :shipping_day_id, numericality: { other_than: 0 , message: "can't be blank"}
-  # validates :status_id, numericality: { other_than: 0 , message: "can't be blank"}
-  # validates :price,  numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 1000000, message: "is out of setting range"}
-
   with_options presence: true do
-    validates :product_name, :text
+    validates :product_name, :text, :image
       with_options  numericality: { other_than: 0 , message: "can't be blank"}do
     validates  :area_id, :category_id, :cost_id, :shipping_day_id, :status_id
       end
--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -9,5 +9,10 @@ FactoryBot.define do
     status_id {2}
     price {2000}
     association :user
+
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png', content_type: 'image/png')
+    end
   end
 end
