4507600044db556518e1c88174657d161745f17b
torennchi <xiangximachang@gmail.com>
2021-09-01 12:40:27 +0900

    修正3

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -4,11 +4,8 @@ window.addEventListener('load', () => {
       const inputValue = priceInput.value;
       const addTaxDom = document.getElementById("add-tax-price");
       addTaxDom.innerHTML = (Math.floor(inputValue * 0.1));
-      console.log(addTaxDom);
       const profitNumber = document.getElementById("profit")
       const value_result = inputValue * 0.1
-      console.log(value_result)
       profitNumber.innerHTML = (Math.floor(inputValue - value_result));
-      console.log(profitNumber);
    })
 });
